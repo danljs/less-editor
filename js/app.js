dfd4f47970d58c89a3f9e31a6424ee49f6dbf9d2
danielliu1973 <daniel.liu1973@gmail.com>
2016-08-19 23:17:57 -0400

    initial

---
+++ js/app.js
@@ -0,0 +1,50 @@
+var model = (function(){
+	var ajax = function(type, url, data){
+        return new Promise(function(resolve, reject){
+            var request = new XMLHttpRequest();
+            request.open(type, url);
+            request.onreadystatechange = function (){
+                if(request.readyState == 2||request.readyState == 3){
+                    //do nothing
+                }
+                else if (request.readyState == 4 && (request.status === 200 || request.status === 204) && resolve) {
+                    resolve(request);
+                }
+                else if (reject) {
+                    reject(request);
+                }
+            }
+            request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+
+            request.onerror = function(){
+                reject(Error('Network Error'));
+            };
+
+            request.send(data);
+        });
+    }
+
+	return{
+		update : function(background, color){
+			ajax('post', 'update', JSON.stringify({
+                color: color,
+                background: background
+            }))
+        .then(
+            function(e){
+            	window.location.reload(false)
+            },
+            function (e) {
+            	console.log(e)
+            })
+		}
+	}
+}());
+
+//================================================================
+function $(id){return document.getElementById(id)}
+(function(){
+	$('button-update').addEventListener('click', function(e){
+		model.update($('input-background').value,$('input-color').value);
+	});
+})();
---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict'
+module.exports = (() => {
+  let express = require("express"),
+      fs = require('fs'),
+      app     = express(),
+      path    = require("path"),
+      less = require('less'),
+      bodyParser = require('body-parser')
+
+  app.use(bodyParser.json());
+  app.use(express.static('.'))
+
+  app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/main.html'))
+  });
+
+  app.post('/update',function(req,res){
+    var input = fs.readFileSync(__dirname + '/less/app.less', 'utf8')
+    var options = {
+      modifyVars: {
+        'color': req.body.color||'#000',
+        'background': req.body.background||'#fff'
+      }
+    }
+
+    less.render(input, options, function (err, result) {
+      if (!err) fs.writeFile(__dirname + '/css/app.css',result.css)
+    });
+
+    // res.sendFile(path.join(__dirname+'/main.html'))
+  });
+
+  let port = 1234
+  app.listen(port);
+  console.log(`Application worker ${process.pid} started at port: ${port}...`)
+})()
